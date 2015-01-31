01c532491c1700df50e392cc067a8974be9283c8
Adam Voss <vossad01@gmail.com>
2015-01-31 09:34:31 -0600

    Adds the 'OwinHarness' project to demonstrate how 'Owin.Extras.MSOwin' may be used.

---
+++ OwinHarness/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace OwinHarness
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // You can use any compatible OWIN host
+            // Self-hosting is used here in order to make it easier to automatically make requests on startup
+            using (WebApp.Start<Startup>("http://localhost:58593"))
+            {
+                PerformExampleClientRequests().Wait();
+
+                Console.WriteLine("Press any key to shutdown.");
+                Console.ReadKey();
+            }
+        }
+
+        private static async Task PerformExampleClientRequests()
+        {
+            var httpClient = new HttpClient();
+
+            await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, "http://localhost:58593/error"));
+
+            await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, "http://localhost:58593/hello/world"));
+
+            await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, "http://localhost:58593/missing/method"));
+        }
+    }
+}
---
+++ OwinHarness/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+
+using Microsoft.Owin;
+using Microsoft.Owin.Logging;
+
+using Owin;
+using OwinHarness;
+
+using Serilog;
+using Serilog.Extras.MSOwin;
+
+using ILogger = Serilog.ILogger;
+using LoggerFactory = Serilog.Extras.MSOwin.LoggerFactory;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace OwinHarness
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ILogger logger =
+                new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(outputTemplate: "{Timestamp} [{Level}] ({RequestId}) {Message}{NewLine}{Exception}")
+                    .CreateLogger();
+
+            logger.Information("OWIN Pipline initilizing");
+
+            // This causes Serilog to capture logging done with to Microsoft's built-in OWIN logging infrastructure
+            app.SetLoggerFactory(new LoggerFactory(logger));
+
+            // This assigns a request context which is available to logging done by middleware following
+            // this registration.  For this to work you need to remember to enrich from the log context
+            app.UseSerilogRequestContext();
+
+            app.UseErrorPage();
+
+            app.Map("/error", builder =>
+            {
+                builder.Run(context =>
+                {
+                    throw new ApplicationException("Note how this is logged by the error page.");
+                });
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+                logger.Information(
+                    "Request for {path} completed with status code {statusCode}",
+                    context.Request.Path,
+                    context.Response.StatusCode);
+            });
+
+            app.Run(async context =>
+            {
+                if (context.Request.Path.StartsWithSegments(new PathString("/hello")))
+                {
+                    var path = context.Request.Path.ToString();
+                    var name = path.Substring(path.IndexOf('/', 1) + 1);
+
+                    logger.Information("Saying Hello to {name}", name);
+                    await context.Response.WriteAsync("Hello " + name);
+                }
+                else
+                {
+                    context.Response.StatusCode = (int) HttpStatusCode.NotFound;
+                }
+            });
+
+            logger.Information("OWIN Pipline initialization complete");
+        }
+    }
+}
